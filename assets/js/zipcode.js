77d7dbfc46338f0e4b89e5d724891db2ce3d3ba2
Cameron Schlueter <118134126+cameron-schlueter@users.noreply.github.com>
2023-01-13 02:48:52 +0000

    recommiting just for update save

--- assets/js/zipcode.js
+++ assets/js/zipcode.js
@@ -1,6 +1,5 @@
-
-const input = document.getElementById("pac-input");
-const submitBtn = document.getElementById("zipcodeSearch");
+const input = document.getElementById("#pac-input");
+const submitBtn = document.getElementById("#zipcodeSearch");
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v12',
@@ -20,15 +19,27 @@ map.on('load', () => {
   });
 });
 
-submitBtn.addEventListener("click", e => {
+submitBtn.addEventListener("click", (e) => {
+ console.log ('button click')
   e.preventDefault();
   const value = input.value;
   console.log(value);
 });
+
+let inputField = document.getElementById('pac-input');
+inputField.addEventListener("blur", function () {
+    localStorage.setItem("input-field", inputField.value);
+});
+
+  // local storage parse
+  localStorage.setItem("object", JSON.stringify(myObject));
+  let myObject = JSON.parse(localStorage.getItem("object"));
   
+//local storage
   const userData = {
     zipcode: document.getElementById('pac-inputs')
   };
+
 
   $.ajax({
     type: "GET",
@@ -38,7 +49,6 @@ submitBtn.addEventListener("click", e => {
     }
   });
   
-
 // possibly not the right url for the endpoint that we need, just a filler currently
   fetch('https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/auto/300x200?access_token=sk.eyJ1IjoiY2Ftc2NobHVldGVyIiwiYSI6ImNsY3R0OHJvMTEzNTgzcGs2MHV6bGFxa3EifQ.M4kwV5JtsfhXnaHPyuyJow', {
     method: 'GET',
