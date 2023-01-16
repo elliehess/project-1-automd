aa2de91e4defb7a782188bf2d840c1594be32836
Cameron Schlueter <118134126+cameron-schlueter@users.noreply.github.com>
2023-01-16 17:01:59 +0000

    Added inline css for the zipcode map. Realized MapBox works on a two key system, so made the access token in HTML script the public key. Made the autoload coordinates Grand Rapids MI. added const zipArray parse/stringify. Commented out lots of js to work through the loading issues, please keep incase. Moved map src script ABOVE the script for the map for cascading purposes. Added if statement to the submitBtn event listener for if zip array is not present.

--- assets/js/zipcode.js
+++ assets/js/zipcode.js
@@ -1,12 +1,18 @@
-const input = document.getElementById("#pac-input");
-const submitBtn = document.getElementById("#zipcodeSearch");
-const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-74.5, 40],
-  zoom: 9
-});
+// only use hash with query selector
+const input = document.getElementById("pac-input");
+const submitBtn = document.getElementById("zipcodeSearch");
+// this creates a list, we want parse to be default
+const zipArray = JSON.parse(localStorage.getItem("zipArray")) || []
 
+//switch over to this one
+// const map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v12',
+//   center: [-74.5, 40],
+//   zoom: 9
+// });
+
+//screen load by layer
 map.on('load', () => {
   map.addLayer({
     id: 'terrain-data',
@@ -19,49 +25,44 @@ map.on('load', () => {
   });
 });
 
+//submit button event listener, start of logic
+
 submitBtn.addEventListener("click", (e) => {
  console.log ('button click')
   e.preventDefault();
   const value = input.value;
-  console.log(value);
-});
+  if (!zipArray.includes(value)) {
+    zipArray.push(value)
+    localStorage.setItem("zipArray", JSON.stringify(zipArray))
 
-let inputField = document.getElementById('pac-input');
-inputField.addEventListener("blur", function () {
-    localStorage.setItem("input-field", inputField.value);
-});
-
-  // local storage parse
-  localStorage.setItem("object", JSON.stringify(myObject));
-  let myObject = JSON.parse(localStorage.getItem("object"));
+  }
   
-//local storage
+});
+
+// input field for zipcode, do we need blur?
+//let inputField = document.getElementById('pac-input');
+// inputField.addEventListener("blur", function () {
+//     localStorage.setItem("input-field", inputField.value);
+// });
+  
+//local storage, does this need to be parsed or stringified?
   const userData = {
     zipcode: document.getElementById('pac-inputs')
   };
 
-
-  $.ajax({
-    type: "GET",
-    url: "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/auto/300x200?access_token=sk.eyJ1IjoiY2Ftc2NobHVldGVyIiwiYSI6ImNsY3R0OHJvMTEzNTgzcGs2MHV6bGFxa3EifQ.M4kwV5JtsfhXnaHPyuyJow",
-    success: function(data) {
-      console.log(data);
-    }
-  });
-  
 // possibly not the right url for the endpoint that we need, just a filler currently
-  fetch('https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/auto/300x200?access_token=sk.eyJ1IjoiY2Ftc2NobHVldGVyIiwiYSI6ImNsY3R0OHJvMTEzNTgzcGs2MHV6bGFxa3EifQ.M4kwV5JtsfhXnaHPyuyJow', {
-    method: 'GET',
-    body: JSON.stringify(userData),
-    Headers: {'Content-Type': 'application/json'},
-  })
-  .then((Response) => Response.json())
-  .then((data) => {
-// is anything special needed here for success besides 'data'?
-    console.log('Success:', data);
-  })
-   .catch((error) => {
-    console.error('Error', error);
-   });
+  // fetch('https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/auto/300x200?access_token=sk.eyJ1IjoiY2Ftc2NobHVldGVyIiwiYSI6ImNsY3R0OHJvMTEzNTgzcGs2MHV6bGFxa3EifQ.M4kwV5JtsfhXnaHPyuyJow', {
+  //   method: 'GET',
+  //   body: JSON.stringify(userData),
+  //   Headers: {'Content-Type': 'application/json'},
+  // })
+  // .then((Response) => Response.json())
+  // .then((data) => {
+
+  //   console.log('Success:', data);
+  // })
+  //  .catch((error) => {
+  //   console.error('Error', error);
+  //  });
 
   // call back function??
